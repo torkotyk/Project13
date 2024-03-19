22cde549caaab4dfd0d04411000d407df76a170a
jabkosmall <jabkosmall@JABKOSMALLE5DB>
2024-03-19 09:22:54 +0200

    Добавьте файлы проекта.

---
+++ Project13/MyForm.cpp
@@ -0,0 +1,11 @@
+#include "MyForm.h"
+#include <Windows.h>
+using namespace Project13; // Project name
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+	return 0;
+}
+
+
